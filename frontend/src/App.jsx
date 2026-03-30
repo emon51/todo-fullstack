5042659cc735d60d2a8368b23a5da89a6ca64b37
emon51 <emanur11235@gmail.com>
2026-03-30 12:44:57 +0600

    Setup frontend project with tailwind css and dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
-
-function App() {
+export default function App() {
   return (
-    <h1>Hello, World</h1>
+    <div className="min-h-screen bg-gray-950 text-white flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Todo Application</h1>
+    </div>
   )
 }
-
-export default App;
